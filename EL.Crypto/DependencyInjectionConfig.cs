37e52133faa88c0d4217ee662f27e2c60bd2c753
Allan Stewart <allans@emmersionlearning.com>
2020-05-15 09:49:33 -0600

    Add DI config

---
+++ EL.Crypto/DependencyInjectionConfig.cs
@@ -0,0 +1,16 @@
+﻿using EL.Crypto.Generators;
+using EL.Crypto.Hashing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EL.Crypto
+{
+    public class DependencyInjectionConfig
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IByteGenerator, ByteGenerator>();
+            services.AddTransient<ITokenGenerator, TokenGenerator>();
+            services.AddTransient<IHasher, Sha256Hasher>();
+        }
+    }
+}
